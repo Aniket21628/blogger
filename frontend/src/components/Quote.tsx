17a682394fe2d980107940c94894914b74537a95
Aniket Palarapu <aniket.palarapu15@gmail.com>
2025-06-25 21:19:33 +0530

    Refactor components: update BlogSkeleton, FullBlog, Quote, Spinner, EditorPreview, PostblogBtn, and Settings for improved functionality and structure

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -1,19 +1,42 @@
+import { useEffect, useState } from "react";
+import { useRandomQuote } from "../hooks";
+interface Quote {
+	text: string;
+	author: string;
+}
 
-export const Quote = () => {
-    return <div className="bg-slate-200 h-screen flex justify-center flex-col">
-        <div className="flex justify-center">
-            <div className="max-w-lg">
-                <div className="text-3xl font-bold">
-                    "The customer support I received was exceptional. The support team went above and beyond to address my concerns"
-                </div>
-                <div className="max-w-md text-xl font-semibold text-left mt-4">
-                    Julies Winfield
-                </div>
-                <div className="max-w-md text-sm font-light text-slate-400">
-                    CEO | Acme corp
-                </div>
-            </div>
-        </div>
-        
-    </div>
+export default function Quote() {
+	const [quote, setQuote] = useState<Quote>();
+
+	useEffect(() => {
+		const fetchQuote = async () => {
+			try {
+				const randomQuote = await useRandomQuote();
+				setQuote(randomQuote);
+			} catch (error) {
+				console.error("Error fetching quote:", error);
+			}
+		};
+
+		fetchQuote();
+	}, []);
+	return (
+		<div className="flex items-center justify-center h-screen text-3xl font-extrabold bg-slate-200">
+			<div className="w-3/4">
+				{quote ? (
+					<>
+						<div>"{quote.text}"</div>
+						<div className="mt-4 text-2xl font-semibold">
+							{quote.author.substring(
+								0,
+								quote.author.indexOf(",")
+							)}
+						</div>
+					</>
+				) : (
+					<div>Loading...</div>
+				)}
+			</div>
+		</div>
+	);
 }
